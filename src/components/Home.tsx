8948598ad45907330cb8897fe42ab47713c3999a
hae8064 <hae8064@naver.com>
2023-11-21 23:26:45 +0900

    design: tailwind 적용 및 초기화

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">PortFolio!</h1>
+    </div>
+  );
+}
+
+export default Home;
